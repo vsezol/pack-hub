7af792e28f0d3c8e1461910f4cd3a9ac9d1d6c8a
vsezol <vsevolod.zolotov02@mail.ru>
2020-07-15 13:03:21 +0700

    Header

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="sass">
+  @font-face
+    font-family: 'Roboto-Regular'
+    src: url('./assets/fonts/Roboto/Roboto-Regular.ttf')
+  @font-face
+    font-family: 'Roboto-Bold'
+    src: url('./assets/fonts/Roboto/Roboto-Bold.ttf')
+
+  *
+    font-family: 'Roboto-Regular'
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#" class="d-flex flex-inline align-items-center brand">
+      Pack&nbsp;
+      <b-badge variant="warning" class="ml-1 brand">hub</b-badge>
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<style lang="sass" scoped>
+  .brand
+    font-family: 'Roboto-Bold'
+    font-size: 1.25rem
+</style>
