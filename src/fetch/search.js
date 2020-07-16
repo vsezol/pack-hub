5d402b21bb7c0753cc76943dbef69f65d6fee457
vsezol <vsevolod.zolotov02@mail.ru>
2020-07-16 13:35:38 +0700

    switch to http

--- src/fetch/search.js
+++ src/fetch/search.js
@@ -1,5 +1,5 @@
 export default async (text, from, size) => {
-  const base = 'http://registry.npmjs.com/-/v1/search'
+  const base = 'https://registry.npmjs.com/-/v1/search'
   const response = await fetch(`${base}?text=${text}&from=${from}&size=${size}`)
   const { total, objects } = await response.json()
   return { total, objects }
